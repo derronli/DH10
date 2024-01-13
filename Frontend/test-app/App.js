1d801683c904e8a508c39dd67b7f5bea4cd2c0de
Baoze Lin <linofbaoze@outlook.com>
2024-01-13 16:01:44 -0500

    added hooks

--- Frontend/test-app/App.js
+++ Frontend/test-app/App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './src/screens/Home'
+import ChosenTask from './src/screens/ChosenTask'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  // global state management
+  const [toDoList, setToDoList] = useState([{id: 1, task:'brush your teeth' }])
+  const [task, setTask] = useState('')
+  const [chosenTask, setChosenTask] = useState('')
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Open up App.js to startqfwefwef testing on your app!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
