f43199fad6d4e96ec228b7adbf73e0876549ff34
Baoze Lin <linofbaoze@outlook.com>
2024-01-13 18:23:16 -0500

    react native elements library installed and expanding journal entry

--- Frontend2.0/my-app/app/(tabs)/four.tsx
+++ Frontend2.0/my-app/app/(tabs)/four.tsx
@@ -1,14 +1,53 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Pressable } from 'react-native';
+import React, { useState } from 'react';
+import { Card } from '@rneui/themed';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { Text, View, } from '../../components/Themed';
 
-export default function TabTwoScreen() {
+const PressableCard = () => {
+
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handlePress = () => {
+    console.log('Card Pressed!');
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <Pressable onPress={handlePress}>
+        <Card>
+            <Card.Title style={styles.title}>Journey Entry</Card.Title>
+            <Card.Divider />
+                {isExpanded ? (
+              <View>
+                <Text>
+                  Expanded Content
+                  A lot more stuff
+                  qofijwef
+                </Text>
+                {/* Add additional content for expanded state */}
+              </View>
+            ) : (
+              <View>
+                <Text>Collapsed Content</Text>
+                {/* Add additional content for collapsed state */}
+              </View>
+            )}
+          </Card>
+      </Pressable>
+  );
+};
+
+export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Three</Text>
+      <Text style={styles.title}>Home Screen</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/three.tsx" />
+      <EditScreenInfo path="app/(tabs)/index.tsx" />
+
+      <PressableCard></PressableCard>
+      
     </View>
   );
 }
