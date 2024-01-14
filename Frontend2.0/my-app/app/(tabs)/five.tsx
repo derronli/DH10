48f6f43ae4eba33e1ec233f36b171e9c5b5843f8
Baoze Lin <linofbaoze@outlook.com>
2024-01-14 02:48:42 -0500

    struggle with llms

--- Frontend2.0/my-app/app/(tabs)/five.tsx
+++ Frontend2.0/my-app/app/(tabs)/five.tsx
@@ -1,16 +1,46 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import { useWhisper } from '@chengsokdara/use-whisper'
 
 export default function TabTwoScreen() {
+  const {
+    recording,
+    speaking,
+    transcribing,
+    transcript,
+    pauseRecording,
+    startRecording,
+    stopRecording,
+  } = useWhisper({
+    apiKey: process.env.EXPO_PUBLIC_OPENAI_API_TOKEN, // YOUR_OPEN_AI_TOKEN
+  })
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Three</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/three.tsx" />
+    <View>
+      <Text>Recording: {recording}</Text>
+      <Text>Speaking: {speaking}</Text>
+      <Text>Transcribing: {transcribing}</Text>
+      <Text>Transcribed Text: {transcript.text}</Text>
+      <TouchableOpacity onPress={() => {startRecording(); console.log('qwef')}}>
+        <View style={styles.button}>
+          <Text>Start</Text>
+          
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => pauseRecording()}>
+        <View style={styles.button}>
+          <Text>Pause</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => stopRecording()}>
+        <View style={styles.button}>
+          <Text>Stop</Text>
+        </View>
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -27,5 +57,11 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    margin: 5,
+    borderRadius: 5,
   },
 });
