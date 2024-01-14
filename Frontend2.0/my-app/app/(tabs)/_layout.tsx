e9a0df7cdc869d3f2f71ff4a417112e1c5bc4071
Derron Li <104483680+derronli@users.noreply.github.com>
2024-01-13 22:50:37 -0500

    did some colour stuff

--- Frontend2.0/my-app/app/(tabs)/_layout.tsx
+++ Frontend2.0/my-app/app/(tabs)/_layout.tsx
@@ -1,14 +1,14 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Tabs } from "expo-router";
+import { Pressable, useColorScheme } from "react-native";
 
-import Colors from '../../constants/Colors';
+import Colors from "../../constants/Colors";
 
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof FontAwesome>["name"];
   color: string;
 }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
@@ -20,26 +20,27 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-      }}>
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+      }}
+    >
       <Tabs.Screen
         name="one"
         options={{
-          title: 'Settings',
+          title: "Settings",
           tabBarIcon: ({ color }) => <TabBarIcon name="cog" color={color} />,
         }}
       />
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Actionables',
+          title: "Actionables",
           tabBarIcon: ({ color }) => <TabBarIcon name="star" color={color} />,
         }}
       />
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: "Home",
           tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
@@ -48,7 +49,7 @@ export default function TabLayout() {
                   <FontAwesome
                     name="info-circle"
                     size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
+                    color={Colors[colorScheme ?? "light"].text}
                     style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
                   />
                 )}
@@ -60,14 +61,14 @@ export default function TabLayout() {
       <Tabs.Screen
         name="four"
         options={{
-          title: 'Journals',
+          title: "Journals",
           tabBarIcon: ({ color }) => <TabBarIcon name="list" color={color} />,
         }}
       />
       <Tabs.Screen
         name="five"
         options={{
-          title: 'You',
+          title: "You",
           tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
         }}
       />
--- Frontend2.0/my-app/app/(tabs)/two.tsx
+++ Frontend2.0/my-app/app/(tabs)/two.tsx
@@ -1,11 +1,28 @@
 import { ScrollView, StyleSheet } from "react-native";
 
-import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
 import { Image, Button, TouchableOpacity } from "react-native";
+import ActionableItem from "../../components/ActionableItem";
+import { useState } from "react";
 
 // THIS IS ACTIONABLES SCREEN =========================================================================
 export default function TabTwoScreen() {
+  const [actionItems, setActionItems] = useState<string[]>([
+    "Wash the dishes",
+    "Clean my room",
+    "Eat food",
+    "Do Homework :(",
+  ]);
+
+  const handleBtnPress = (txt) => {
+    let cp = [...actionItems];
+    let i = cp.indexOf(txt);
+    if (i != -1) {
+      cp.splice(i, 1);
+      setActionItems(cp);
+    }
+  };
+
   return (
     <ScrollView style={{ backgroundColor: "white" }}>
       <View style={styles.section1}>
@@ -25,12 +42,9 @@ export default function TabTwoScreen() {
         />
       </View>
       <View style={styles.section2}>
-        <View style={card.taskContainer}>
-          <Text style={card.txt}>Wash the Dishes</Text>
-          <TouchableOpacity style={card.btn}>
-            <Text>Done</Text>
-          </TouchableOpacity>
-        </View>
+        {actionItems.map((txt, index) => (
+          <ActionableItem key={index} text={txt} onBtnPress={handleBtnPress} />
+        ))}
       </View>
     </ScrollView>
   );
@@ -53,32 +67,5 @@ const styles = StyleSheet.create({
   img: {
     width: 100,
     height: 100,
-  },
-});
-
-const card = StyleSheet.create({
-  taskContainer: {
-    backgroundColor: "white",
-    paddingVertical: 20,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    borderBottomColor: "black",
-    borderBottomWidth: 1,
-    marginHorizontal: 20,
-  },
-  btn: {
-    elevation: 8,
-    backgroundColor: "#FF6961",
-    borderRadius: 10,
-    paddingVertical: 8,
-    paddingHorizontal: 14,
-  },
-  separator: {
-    height: 1,
-    width: "80%",
-  },
-  txt: {
-    color: "black",
   },
 });
