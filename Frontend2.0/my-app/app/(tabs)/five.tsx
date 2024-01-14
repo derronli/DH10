86cc5770ae1c0c60c45912438d9e19cdcdf2f9ec
HagOrMan <khagypi3@gmail.com>
2024-01-14 09:13:33 -0500

    Added mental health accordion to 'You' tab

--- Frontend2.0/my-app/app/(tabs)/five.tsx
+++ Frontend2.0/my-app/app/(tabs)/five.tsx
@@ -1,11 +1,50 @@
-import { StyleSheet, Image } from "react-native";
+import { ScrollView, StyleSheet, Image } from "react-native";
 
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
 
-import { useNavigation, useRouter, useLocalSearchParams } from "expo-router";
+import { ListItem } from '@rneui/themed';
 
-export default function TabTwoScreen() {
+import { useNavigation, useRouter, useLocalSearchParams } from "expo-router";
+import { useState } from "react";
+
+import MentalHealthInfo from "../../mental_health/health_resources";
+
+function MentalHealthAccordion(): any {
+
+  const [expanded, setExpanded] = useState(false);
+
+  return <ListItem.Accordion
+  content={
+    <>
+      <ListItem.Content>
+        <ListItem.Title>List Accordion</ListItem.Title>
+      </ListItem.Content>
+    </>
+  }
+  isExpanded={expanded}
+  onPress={() => {
+    setExpanded(!expanded);
+  }}
+>
+  <ListItem bottomDivider>
+    <ListItem.Content>
+      <ListItem.Title>Mental Health Resources</ListItem.Title>
+      <ListItem.Subtitle>{MentalHealthInfo.Helpline}</ListItem.Subtitle>
+    </ListItem.Content>
+    <ListItem.Chevron />
+  </ListItem>
+  <ListItem bottomDivider>
+    <ListItem.Content>
+      <ListItem.Title>Mental Health Tips</ListItem.Title>
+      <ListItem.Subtitle>{MentalHealthInfo.Tips}</ListItem.Subtitle>
+    </ListItem.Content>
+    <ListItem.Chevron />
+  </ListItem>
+</ListItem.Accordion>
+}
+
+export default function TabFiveScreen() {
   const navigation = useNavigation();
   const router = useRouter();
   const params = useLocalSearchParams();
@@ -18,16 +57,18 @@ export default function TabTwoScreen() {
   ];
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Three</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <Image source={pics[id]} style={{ width: 100, height: 100 }} />
-      <EditScreenInfo path="app/(tabs)/three.tsx" />
-    </View>
+    <ScrollView style={{ backgroundColor: "white" }}>
+      <View style={styles.container}>
+        <Text style={styles.title}>You</Text>
+        <View
+          style={styles.separator}
+          lightColor="#eee"
+          darkColor="rgba(255,255,255,0.1)"
+        />
+        <Image source={pics[id]} style={{ width: 100, height: 100 }} />
+        
+      </View>
+    </ScrollView>
   )
 }
 
