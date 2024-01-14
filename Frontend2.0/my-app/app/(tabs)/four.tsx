9bb8965fd625072cfa822ea402be9272139a7ac6
Derron Li <104483680+derronli@users.noreply.github.com>
2024-01-13 19:10:48 -0500

    Implemented state to correctly display JournalEntry in 2 columns

--- Frontend2.0/my-app/app/(tabs)/four.tsx
+++ Frontend2.0/my-app/app/(tabs)/four.tsx
@@ -1,33 +1,37 @@
 import { StyleSheet } from "react-native";
 
-import EditScreenInfo from "../../components/EditScreenInfo";
+import JournalEntry from "../../components/JournalEntry";
 import { Text, View } from "../../components/Themed";
+import { Button } from "react-native";
+import { useState } from "react";
 
 export default function TabTwoScreen() {
+  const [journals, setJournals] = useState<number[]>([]);
+
+  const handleAddJournalEntry = () => {
+    // The list will just be some values for now -> later on will need to change
+    // Adding the value 0 to the array
+    setJournals((journals) => [...journals, 0]);
+    console.log(journals);
+  };
+
   return (
     <View>
-      <View>
+      <Button onPress={handleAddJournalEntry} title="Add" />
+      <View style={{ flexDirection: "row" }}>
+        {/* This is the left column */}
         <View style={styles.boxContainer}>
-          <View style={styles.box1}>
-            <View style={styles.textbox}>
-              <Text style={styles.heading}>Bad Day</Text>
-              <Text style={styles.note}>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Repudiandae laudantium ipsum ab possimus esse maxime atque,
-              </Text>
-              <Text style={{ textAlign: "right", color: "black" }}>
-                Jan 13, 2024
-              </Text>
-            </View>
-          </View>
-          <View style={styles.box2}>
-            <Text>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Repudiandae laudantium ipsum ab possimus esse maxime atque,
-              dolorum et tempora porro harum quas laboriosam excepturi suscipit
-              voluptas vero sint consequatur eligendi.
-            </Text>
-          </View>
+          {/* Only rendering even index journal entries */}
+          {journals.map((journal, index) =>
+            index % 2 === 0 ? <JournalEntry key={index} /> : null
+          )}
+        </View>
+
+        {/* This is the right column */}
+        <View style={styles.boxContainer}>
+          {journals.map((journal, index) =>
+            index % 2 === 1 ? <JournalEntry key={index} /> : null
+          )}
         </View>
       </View>
     </View>
@@ -36,31 +40,7 @@ export default function TabTwoScreen() {
 
 const styles = StyleSheet.create({
   boxContainer: {
-    flexDirection: "row",
-  },
-  box1: {
+    flexDirection: "column",
     flex: 1,
-    backgroundColor: "#77DD77",
-    marginHorizontal: 10,
-    borderRadius: 8,
-  },
-  box2: {
-    flex: 1,
-    backgroundColor: "#FF0000",
-    marginHorizontal: 10,
-  },
-  textbox: {
-    backgroundColor: "transparent",
-    marginHorizontal: 13,
-    marginVertical: 15,
-  },
-  heading: {
-    marginBottom: 10,
-    color: "white",
-    fontWeight: "bold",
-  },
-  note: {
-    color: "black",
-    marginBottom: 10,
   },
 });
