542d84baff0c1ff2e948f69ced16ede956b5b698
Baoze Lin <linofbaoze@outlook.com>
2024-01-13 21:43:25 -0500

    rebase 1

--- Frontend2.0/my-app/app/(tabs)/index.tsx
+++ Frontend2.0/my-app/app/(tabs)/index.tsx
@@ -1,12 +1,105 @@
 import { Image, TextInput, StyleSheet, ImageBackground } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
+import React, { useState, useEffect } from 'react';
+import { Audio } from 'expo-av';
+import * as FileSystem from 'expo-file-system';
 import { Text, View } from '../../components/Themed';
 import { FullWindowOverlay } from 'react-native-screens';
 
 import {Button} from '@rneui/themed';
 
 export default function JournalScreen() {
+
+  const [recording, setRecording] = useState<Audio.Recording | null>(null);
+  const [recordingStatus, setRecordingStatus] = useState('idle');
+  const [audioPermission, setAudioPermission] = useState<boolean | null>(null);
+
+  useEffect(() => {
+
+    // Simply get recording permission upon first render
+    async function getPermission() {
+      await Audio.requestPermissionsAsync().then((permission) => {
+        console.log('Permission Granted: ' + permission.granted);
+        setAudioPermission(!!permission.granted)
+      }).catch(error => {
+        console.log(error);
+      });
+    }
+
+    // Call function to get permission
+    getPermission()
+    // Cleanup upon first render
+    return () => {
+      if (recording) {
+        stopRecording();
+      }
+    };
+  }, []);
+
+  async function startRecording() {
+    try {
+      // needed for IoS
+      if (audioPermission) {
+        await Audio.setAudioModeAsync({
+          allowsRecordingIOS: true,
+          playsInSilentModeIOS: true
+        })
+      }
+
+      const newRecording = new Audio.Recording();
+      console.log('Starting Recording')
+      // Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY revmoed
+      await newRecording.prepareToRecordAsync();
+      await newRecording.startAsync();
+      setRecording(newRecording);
+      setRecordingStatus('recording');
+
+    } catch (error) {
+      console.error('Failed to start recording', error);
+    }
+  }
+
+  async function stopRecording() {
+    try {
+
+      if (recording && recordingStatus === 'recording') {
+        console.log('Stopping Recording')
+        await recording.stopAndUnloadAsync();
+        const recordingUri = recording.getURI();
+        // Create a file name for the recording
+        const fileName = `recording-${Date.now()}.wav`;
+        if (recordingUri) {
+
+          // Move the recording to the new directory with the new file name
+          await FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'recordings/', { intermediates: true });
+          await FileSystem.moveAsync({
+            from: recordingUri,
+            to: FileSystem.documentDirectory + 'recordings/' + `${fileName}`
+          });
+        }
+
+        // This is for simply playing the sound back
+        const playbackObject = new Audio.Sound();
+        await playbackObject.loadAsync({ uri: FileSystem.documentDirectory + 'recordings/' + `${fileName}` });
+        await playbackObject.playAsync();
+
+        // resert our states to record again
+        setRecording(null);
+        setRecordingStatus('stopped');
+      }
+
+    } catch (error) {
+      console.error('Failed to stop recording', error);
+    }
+  }
+
+  async function handleRecordButtonPress() {
+    if (recording) {
+      const audioUri = await stopRecording();
+      console.log('Saved audio file to', audioUri);
+    } else {
+      await startRecording();
+    }
+  }
 
   const image = require('../../assets/images/beach.jpg');
 
@@ -28,17 +121,13 @@ export default function JournalScreen() {
 
           <View style={styles.button}>
             <Button
-              onPress={() => {
-                console.log("started recording")
-              }}
-              title="Start Speaking"
+              onPress={handleRecordButtonPress}
+              title={recording ? 'Recording' : 'Not Recording'}
               color="#FF3D00"
               size='lg'
               titleStyle={{fontSize:18}}
             />
           </View> 
-
-          
         </View>
       </ImageBackground>
     // </View>
